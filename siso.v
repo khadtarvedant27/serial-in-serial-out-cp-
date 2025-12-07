bdb5af663fbba8fe7855b6913c5c0f7f0149982e
Vedant Khadtar <khadtarvedant27@gmail.com>
2025-12-08 00:16:06 +0530

    Create siso.v

---
+++ siso.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module siso(clk,rst,sin,sout );
+input clk,sin,rst;
+output sout;
+reg[3:0]q;
+always @(posedge clk or posedge rst)
+begin
+if(rst)
+q<=4'b0000;
+else
+q<={q[2:0],sin};
+end
+assign sout=q[3];
+endmodule
